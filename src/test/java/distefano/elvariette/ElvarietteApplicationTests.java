dba9515065bc72f79a531f89d4bf6f49d32e4f38
Udistefano <udistefano@fi.uba.ar>
2026-04-10 18:21:34 -0300

    agrego proyecto springboot

---
+++ src/test/java/distefano/elvariette/ElvarietteApplicationTests.java
@@ -0,0 +1,13 @@
+package distefano.elvariette;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElvarietteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
